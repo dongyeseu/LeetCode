6a8620175b11e560579231d79de97f89fcaf1841
Haoran Lou <37174250+Pythonboy@users.noreply.github.com>
2018-07-23 14:23:39 +0800

    House Robber —— 动态规划

---
+++ C-Code/198.cpp
@@ -0,0 +1,48 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+             
+
+*/
+
+class Solution {
+public:
+    int Max(int a,int b)
+    {
+        return a>b?a:b;
+    }
+    
+    int solve(vector<int>&nums,int N,int res[])
+    {
+        if(N<0)
+            return 0;
+        if(res[N]>=0)
+            return res[N];         //关键
+        res[N] = Max(nums[N]+solve(nums,N-2,res),solve(nums,N-1,res));    //关键
+        return res[N];
+    }
+    
+    int rob(vector<int>& nums) {
+        int len = nums.size();
+        if(len == 0)
+            return 0;
+        int res[len];
+        for(int i = 0 ; i<len ; i++)
+            res[i] = -1;
+        return solve(nums,len-1,res);
+    }
+};
